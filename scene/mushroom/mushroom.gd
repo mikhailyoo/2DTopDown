cf7ff95ed1a6a5d6f184e2a9ee2c1041c8782f79
mikhailyoo <mikhailyoo@gmail.com>
2024-10-30 13:44:54 +0500

    lesson 10

---
+++ scene/mushroom/mushroom.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+var max_speed = 50
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var direction = get_direction_to_player()
+	velocity = max_speed * direction
+	move_and_slide()
+
+
+func get_direction_to_player():
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	if player != null:
+		return (player.global_position - global_position).normalized()
+	return Vector2(0,0)
+
+
+func _on_area_2d_area_entered(area):
+	queue_free()
--- scene/player/player.gd
+++ scene/player/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+<<<<<<< Updated upstream
 var max_speed = 200
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,10 +12,26 @@ func _process(delta):
 	var movement = movement_vector()
 	var direction = movement.normalized()
 	velocity = max_speed * direction
+=======
+var max_speed = 125
+var acceleration = .15
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var direction = movement_vector().normalized()
+	var target_velocity = max_speed * direction
+	
+	velocity = velocity.lerp(target_velocity, acceleration)
+>>>>>>> Stashed changes
 	move_and_slide()
 #
 
 func movement_vector():
 	var movement_x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var movement_y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+<<<<<<< Updated upstream
 	return Vector2(movement_x,movement_y)
+=======
+	return Vector2(movement_x, movement_y)
+>>>>>>> Stashed changes
