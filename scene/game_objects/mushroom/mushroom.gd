79cdbc4625dbc146418098210123d816c4512380
mikhailyoo <mikhailyoo@gmail.com>
2024-11-19 20:24:57 +0500

    lesson 14

--- scene/game_objects/mushroom/mushroom.gd
+++ scene/game_objects/mushroom/mushroom.gd
@@ -17,7 +17,3 @@ func get_direction_to_player():
 	if player != null:
 		return (player.global_position - global_position).normalized()
 	return Vector2(0,0)
-
-
-func _on_area_2d_area_entered(area):
-	health_component.take_damage(5)
