bdd652d24fc6a2128dbafefcda8a976c8239d492
mikhailyoo <mikhailyoo@gmail.com>
2024-12-05 10:11:01 +0500

    lesson 23

--- scene/manager/arena_time_manager.gd
+++ scene/manager/arena_time_manager.gd
@@ -1,7 +1,15 @@
 extends Node
 
+@export var end_screen_scene: PackedScene
+
 @onready var timer = $Timer
 
 
 func get_time_elapsed():
 	return timer.wait_time - timer.time_left
+
+
+func _on_timer_timeout():
+	var end_screen_instance = end_screen_scene.instantiate() as EndScreen
+	get_parent().add_child(end_screen_instance)
+	end_screen_instance.change_to_victory()
