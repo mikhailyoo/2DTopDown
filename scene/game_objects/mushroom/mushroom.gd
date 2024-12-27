18cc93e15a64fa5515ab22f052ffb2ba4a495539
mikhailyoo <mikhailyoo@gmail.com>
2024-12-27 18:31:29 +0500

    lesson 30

--- scene/game_objects/mushroom/mushroom.gd
+++ scene/game_objects/mushroom/mushroom.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 var max_speed = 50
 
 @onready var health_component = $HealthComponent
-
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 func _ready():
 	health_component.died.connect(on_died)
@@ -12,6 +12,15 @@ func _process(delta):
 	var direction = get_direction_to_player()
 	velocity = max_speed * direction
 	move_and_slide()
+	
+	if direction.x != 0 || direction.y != 0:
+		animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("idle")
+		
+	var face_sign = sign(direction.x)
+	if face_sign != 0:
+		animated_sprite_2d.scale.x = face_sign
 
 
 func get_direction_to_player():
