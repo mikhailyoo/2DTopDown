3c9d18e4eadf1267d6f21ecbf03a62e952b02647
mikhailyoo <mikhailyoo@gmail.com>
2025-01-15 20:03:30 +0500

    lesson 33

--- scene/components/death_component.gd
+++ scene/components/death_component.gd
@@ -2,3 +2,4 @@ extends Node2D
 class_name DeathComp
 
 @onready var gpu_particles_2d = %GPUParticles2D
+@onready var sprite_offset = $SpriteOffset
--- scene/game_objects/mushroom/mushroom.gd
+++ scene/game_objects/mushroom/mushroom.gd
@@ -1,9 +1,8 @@
 extends CharacterBody2D
 
-var max_speed = 50
-
 @onready var health_component = $HealthComponent
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var movement_component = $MovementComponent
 
 @export var death_scene: PackedScene
 @export var sprite: CompressedTexture2D
@@ -12,9 +11,8 @@ func _ready():
 	health_component.died.connect(on_died)
 
 func _process(delta):
-	var direction = get_direction_to_player()
-	velocity = max_speed * direction
-	move_and_slide()
+	var direction = movement_component.get_direction()
+	movement_component.move_to_player(self)
 	
 	if direction.x != 0 || direction.y != 0:
 		animated_sprite_2d.play("run")
@@ -26,17 +24,12 @@ func _process(delta):
 		animated_sprite_2d.scale.x = face_sign
 
 
-func get_direction_to_player():
-	var player = get_tree().get_first_node_in_group("player") as Node2D
-	if player != null:
-		return (player.global_position - global_position).normalized()
-	return Vector2(0,0)
-	
-
 func on_died():
 	var back_layer = get_tree().get_first_node_in_group("back_layer")
 	var death_instance = death_scene.instantiate() as DeathComp
 	back_layer.add_child(death_instance)
 	death_instance.gpu_particles_2d.texture = sprite
+	death_instance.sprite_offset.position.y = animated_sprite_2d.offset.y
+	print(death_instance.sprite_offset.position.y)
 	death_instance.global_position = global_position
 	queue_free()
