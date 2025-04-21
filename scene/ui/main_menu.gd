a8f809ca25c960be088c48697408a47b5540723b
mikhailyoo <mikhailyoo@gmail.com>
2025-04-21 21:00:17 +0500

    lesson 51

---
+++ scene/ui/main_menu.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+
+
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene/level/level.tscn")
+
+
+func _on_options_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
