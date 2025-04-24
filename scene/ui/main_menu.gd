32b7e4fa9ec5bc9a905d9831ba564d23815c109d
mikhailyoo <mikhailyoo@gmail.com>
2025-04-24 18:18:56 +0500

    lesson 52

--- scene/ui/main_menu.gd
+++ scene/ui/main_menu.gd
@@ -1,13 +1,14 @@
 extends CanvasLayer
 
-
+var options_menue_scene = preload("res://scene/ui/options_menu.tscn")
 
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scene/level/level.tscn")
 
 
 func _on_options_button_pressed() -> void:
-	pass # Replace with function body.
+	var options_instance = options_menue_scene.instantiate()
+	add_child(options_instance)
 
 
 func _on_quit_button_pressed() -> void:
