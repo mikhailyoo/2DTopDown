80cc129d8731fd8347000560ce0503f83b1b9cc8
mikhailyoo <mikhailyoo@gmail.com>
2025-05-07 21:34:37 +0500

    lesson 53

--- scene/level/level.gd
+++ scene/level/level.gd
@@ -4,10 +4,16 @@ extends Node
 
 @onready var player = %Player
 
+var pause_menu_scene = preload("res://scene/ui/pause_menu.tscn")
 
 func _ready():
 	MusicPlayer.play()
 	player.health_component.died.connect(on_died)
+	
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		add_child(pause_menu_scene.instantiate())
 	
 	
 func on_died():
