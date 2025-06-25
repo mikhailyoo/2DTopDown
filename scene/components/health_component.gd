f18a41fa6988ff3b8b64c47421057f4949a237c5
mikhailyoo <mikhailyoo@gmail.com>
2025-06-25 22:28:31 +0500

    lesson 62

--- scene/components/health_component.gd
+++ scene/components/health_component.gd
@@ -2,7 +2,8 @@ extends Node
 class_name HealthComponent
 
 signal died
-signal health_changed
+signal health_decreased
+signal health_increased
 
 @export var max_health: float = 10
 @export var damage_text_scene: PackedScene
@@ -20,8 +21,20 @@ func take_damage (damage):
 	damage_text_instance.global_position = owner.global_position
 	damage_text_instance.damage_text(damage)
 	current_health = max(current_health - damage, 0)
-	health_changed.emit()
+	health_decreased.emit()
 	Callable(check_death).call_deferred()
+	
+
+func take_heal(heal):
+	if current_health < max_health:
+		var front_layer = get_tree().get_first_node_in_group("front_layer") as Node2D
+		var damage_text_instance = damage_text_scene.instantiate() as Node2D
+		front_layer.add_child(damage_text_instance)
+		damage_text_instance.global_position = owner.global_position
+		damage_text_instance.modulate = Color("72d6ce")
+		damage_text_instance.damage_text(heal)
+		current_health = min(current_health + heal, max_health)
+		health_increased.emit()
 	
 
 func get_health_value():
